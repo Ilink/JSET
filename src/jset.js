826a6d71757f1e6b4d329fc80cc0a95e89194c49
Ian <ian@linkportfolio.net>
2012-05-30 11:16:38 -0700

    more ideas and interfaces

--- src/jset.js
+++ src/jset.js
@@ -8,30 +8,43 @@ There is a tradeoff for speed using this design, though I have not tested how mu
 The underlying structure looks like this:
 
     set: {
-        "1": 1
-        "2" : 2
-    }
-
-As you can see, every hash has a value of itself. This is redundent, I wonder if I could get away with:
-
-    set : {
         "1": 1,
-        "2": 1,
+        "2" : 1,
+        "3": 1
     }
 
-In principle, this is less memory...
+The "1"s are just placeholder values. There may be a more efficient way of doing this.
  */
 
 function Set(){
     var set;
     set = {};
-    $.each(arguments, function(i, arg){
-        if(typeof set[arg] === 'undefined'){
-            set[arg] = arg;
-        }
-    });
 
-    this.add = function(){
+    /*
+    @function _add
+    Inserts any number of values into the set.
+    Maintains the invariant that set values must be unique.
+     */
+    function _add(){
+        $.each(arguments, function(i, arg){
+            var val = set[arg]
+            if(typeof val === 'undefined'){
+                set[arg] = 1;
+            }
+        });
+    }
 
+    /*
+    @Constructor
+    Accepts any number of arguments, inserts them into set
+     */
+    _add(arguments);
+
+    /*
+    @function insert
+    Accepts any number of arguments. Public alias for _add
+     */
+    this.insert = function(){
+        _add(arguments);
     }
 }
